3e3cec7baf36d90a83e18768057b84508abaaeb7
Kool <kool.dennis@kpmg.nl>
2018-09-07 12:20:02 +0200

    Code cleanup

--- Show-DfsShares.ps1
+++ Show-DfsShares.ps1
@@ -12,7 +12,7 @@ Show-DfsShares -verbose
 Function name:          Show-DfsShares
 Author:                 Dennis Kool
 DateCreated:            27-08-2018
-DateModified:           27-08-2018
+DateModified:           07-09-2018
 
 .NOTES
 27-08-2018:             Initial release
@@ -26,19 +26,34 @@ function Show-DfsShares {
     param()
  
     #load static variables using json file
-    Write-Verbose "[$(Get-Date)] Loading data from JSON"
-    $config = Get-Content `
-        -Path $configfile `
-        -Raw | ConvertFrom-Json
+    if ([string]::IsNullOrEmpty($config)) {
+        Write-Verbose "[$(Get-Date)] Settings config file variable"
+        $params = @{
+            Path = $configfile
+            Raw  = $true}
+            $config = Get-Content @params |
+            ConvertFrom-Json
+    }
 
     #extracting shares
-    Write-Verbose "[$(Get-Date)] Extracting shares from DFS folder paths"
-    $FolderList = foreach ($folder in $config.dfsfolderpaths) {get-dfsnfolder -path $folder} 
-    $DfsFolderTargets = ForEach ($Folder in $FolderList) { 
-        Get-DfsnFolderTarget -Path $Folder.Path 
+    if ([string]::IsNullOrEmpty($FolderList)) {
+        Write-Verbose "[$(Get-Date)] Extracting shares from DFS folder paths"
+        $FolderList = foreach ($folder in $config.dfsfolderpaths) {
+            $params = @{
+                Path = $folder}
+                get-dfsnfolder @params
+        }
+         
+        if ([string]::IsNullOrEmpty($DfsFolderTargets)) {
+            $DfsFolderTargets = ForEach ($Folder in $FolderList) { 
+                $params = @{
+                    Path = $Folder.Path}
+                    Get-DfsnFolderTarget @params 
+            }
+        }
+        if ([string]::IsNullOrEmpty($DfsShares)) {
+            $DfsShares = $DfsFolderTargets | Select-Object Path, TargetPath | Sort-Object TargetPath
+            write-verbose ($DfsShares | Out-String)
+        }
     }
-    $DfsShares = $DfsFolderTargets | Select-Object Path, TargetPath | Sort-Object TargetPath
-
-    write-verbose ($DfsShares | Out-String)
-
 }
